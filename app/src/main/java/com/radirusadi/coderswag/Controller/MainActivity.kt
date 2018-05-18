d4d2a85a0d437c6918c1891a5698f7e339ff05e6
Radi <radi.rusadi.s@gmail.com>
2018-05-18 22:49:54 +0700

    Simple Listview

--- app/src/main/java/com/radirusadi/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/radirusadi/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,25 @@ package com.radirusadi.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.radirusadi.coderswag.Model.Category
 import com.radirusadi.coderswag.R
+import com.radirusadi.coderswag.Service.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                DataService.categories
+        )
+
+        categoryListview.adapter = adapter
     }
 }
--- app/src/main/java/com/radirusadi/coderswag/Model/Category.kt
+++ app/src/main/java/com/radirusadi/coderswag/Model/Category.kt
@@ -2,6 +2,8 @@ package com.radirusadi.coderswag.Model
 
 class Category (val titel: String, val image: String){
 
-
+    override fun toString(): String {
+        return titel
+    }
 
 }
